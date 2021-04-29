a80ab9ae143f4d1f4b89a93e55d52325123f96db
keewee <kinycx@gmail.com>
2021-04-29 02:03:15 +0200

    -added keystone and app release -increased security -improved stability

--- app/src/main/java/com/bro/bellavistabackend/TruckFoodActivity.java
+++ app/src/main/java/com/bro/bellavistabackend/TruckFoodActivity.java
@@ -1,6 +1,7 @@
 package com.bro.bellavistabackend;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -8,6 +9,12 @@ import android.os.Bundle;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -24,71 +31,145 @@ import androidx.appcompat.widget.ActivityChooserView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class TruckFoodActivity extends AppCompatActivity {
+import org.w3c.dom.ls.LSException;
+
+import java.util.Objects;
+
+public class TruckFoodActivity extends FragmentActivity implements OnMapReadyCallback {
 
     DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-    DatabaseReference mConditionRef = mRootRef.child("condition");
+    DatabaseReference mLatRef = mRootRef.child("trackfood1").child("latitude");
+    DatabaseReference mLongRef = mRootRef.child("trackfood1").child("longitude");
+
+    FloatingActionButton mPositionButton;
+    FloatingActionButton mStop;
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int REQUEST_CODE = 101;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setVisibility(View.GONE);
-        setSupportActionBar(toolbar);
+        Context context;
+        mPositionButton = (FloatingActionButton) findViewById(R.id.currentLocation);
+        mStop = (FloatingActionButton) findViewById(R.id.stop);
 
-        Fragment fragment = new MapFragment();
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.frame_layout, fragment)
-                .commit();
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        fetchLastLocation();
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void fetchLastLocation() {
+        if (ActivityCompat.checkSelfPermission(TruckFoodActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            Task<Location> task = fusedLocationProviderClient.getLastLocation();
+            task.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null){
+                        currentLocation = location;
+                        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                        assert supportMapFragment != null;
+                        supportMapFragment.getMapAsync(TruckFoodActivity.this);
+                    }
+                }
+            });
+        } else {
+            ActivityCompat.requestPermissions(TruckFoodActivity.this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
         }
 
-        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+        //floating GPS button
+        mPositionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleMap.clear();
+                MarkerOptions markerOptions = new MarkerOptions().position(latLng)
+                        .title("sono qui");
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                googleMap.addMarker(markerOptions);
+                Snackbar.make(v, "Settata la posizione corrente", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                //icona bitmap
+                //markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.));
+
+                mLatRef.setValue(latLng.latitude);
+                mLongRef.setValue(latLng.longitude);
+            }
+        });
+
+        mStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleMap.clear();
+                Snackbar.make(v, "Fermata la condivisione sul sito", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                mLatRef.setValue(0);
+                mLongRef.setValue(0);
+            }
+        });
+
+
+        //fine location GPS
+        //click su mappa
+
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                assert supportMapFragment != null;
+                Snackbar.make(supportMapFragment.requireView(), "Posizione settata manualmente", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                //quando clicchi sulla mappa
+                MarkerOptions markerOptions1 = new MarkerOptions();
+                //icona bitmap
+                //markerOptions1.icon(BitmapDescriptorFactory.fromResource(R.drawable.));
+                //posizione del marker
+                markerOptions1.position(latLng);
+                //titolo del marker
+                markerOptions1.title(latLng.latitude + " : " + latLng.longitude);
+                //rimuovi i marker
+                googleMap.clear();
+                //animazione
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                //aggiungi marker
+                googleMap.addMarker(markerOptions1);
+
+                //googleMap.setMyLocationEnabled(true);
+
+                mLatRef.setValue(latLng.latitude);
+                mLongRef.setValue(latLng.longitude);
+            }
+        });
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-
-        mConditionRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String lat = snapshot.getValue(String.class);
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    fetchLastLocation();
+                }
+                break;
+        }
     }
 }
